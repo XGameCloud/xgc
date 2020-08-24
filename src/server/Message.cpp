3ca98c8352a1df5065dd4d0b123a0497fafaa6d5
mengxianglong <mengxianglong@xgc.com>
2020-08-24 20:58:04 +0800

    modify class Message

--- src/server/Message.cpp
+++ src/server/Message.cpp
@@ -1,24 +1,49 @@
 #include "Message.h"
-Message::Message(std::string time,MSGTYPE type,std::string moudle,std::string data)
-{
-    set(time,type,moudle,data);
+using message::Message;
+using message::ControlType;
+Message::Message(ControlType conrtol_word,size_t message_number,
+                 unsigned int sender_identity,unsigned int receiver_identity,
+                 std::string data):
+    control_word_(conrtol_word),message_number_(message_number),
+    sender_identity_(sender_identity),receiver_identity_(receiver_identity),data_(data){
+    CalculateDataSize();
+    CalculateCheckSum();
 }
-Message::Message()
+void Message::SetMessageData(std::string data)
 {
-    MSGTYPE type;
-    type=MSGTYPE::Info;
-    set(0,type,"null","null");
-}
-void Message::set(std::string time,MSGTYPE type,std::string moudle,std::string data)
-{
-    this->time=time;
-    this->type=type;
-    this->moudle=moudle;
-    this->data=data;
+    data_ = data;
+    CalculateDataSize();
+    CalculateCheckSum();
 }
 
 template<class Archive>
-void Message::serialize(Archive&ar,const uint version)
+void Message::serialize(Archive &ar,const uint version)
 {
-    ar &time &type &moudle &data;
+    ar & control_word_ & data_size_ & message_number_ & sender_identity_ &
+         receiver_identity_ & check_sum_ & data_ ;
 }
+
+
+// Message::Message(std::string time,MSGTYPE type,std::string moudle,std::string data)
+// {
+//     set(time,type,moudle,data);
+// }
+// Message::Message()
+// {
+//     MSGTYPE type;
+//     type=MSGTYPE::Info;
+//     set(0,type,"null","null");
+// }
+// void Message::set(std::string time,MSGTYPE type,std::string moudle,std::string data)
+// {
+//     this->time=time;
+//     this->type=type;
+//     this->moudle=moudle;
+//     this->data=data;
+// }
+
+// template<class Archive>
+// void Message::serialize(Archive &ar,const uint version)
+// {
+//     ar &time &type &moudle &data;
+// }
